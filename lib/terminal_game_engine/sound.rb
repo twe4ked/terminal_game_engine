9e83392b3ee446c775bf24d971ce281dd0776092
Odin Dutton <odindutton@gmail.com>
2016-07-15 21:31:38 +1000

    Log sound related errors

--- lib/terminal_game_engine/sound.rb
+++ lib/terminal_game_engine/sound.rb
@@ -1,11 +1,19 @@
+require 'open3'
+
 module TerminalGameEngine
   class Sound
-    def self.play(path)
+    def self.play(path, logger: Logger.new('/dev/null'))
       case RUBY_PLATFORM
       when /darwin/
-        system "afplay #{path} &"
+        stdin, stdout, stderr, wait_thr = Open3.popen3('afplay', path)
       when /linux/
-        system "command -v mplayer >/dev/null 2>&1 && mplayer -msglevel all=-1 -nolirc #{path} &"
+        stdin, stdout, stderr, wait_thr = Open3.popen3('command -v mplayer >/dev/null 2>&1 && mplayer -msglevel all=-1 -nolirc', path)
+      end
+
+      Thread.new do
+        while line = stderr.readline
+          logger.error line.chomp
+        end
       end
     end
   end
