1b9905a64be5cbccef48920dcafd7a82f22c5230
Odin Dutton <odindutton@gmail.com>
2016-07-15 21:54:42 +1000

    Start implementing a DSL

--- examples/counter.rb
+++ examples/counter.rb
@@ -1,32 +1,32 @@
 require_relative '../lib/terminal_game_engine'
 
-TerminalGameEngine::Frame.setup
+TerminalGameEngine.run do
+  on_tick do |tick|
+    number = tick.to_s
 
-TerminalGameEngine::Engine.tick do |tick|
-  number = tick.to_s
+    height = 3
+    width = number.length + 2
 
-  height = 3
-  width = number.length + 2
+    frame = TerminalGameEngine::Frame.new width, height
 
-  frame = TerminalGameEngine::Frame.new width, height
+    # draw number
+    frame.draw_center 1, number
 
-  # draw number
-  frame.draw_center 1, number
+    # draw corners
+    frame.draw 0, 0, '+'
+    frame.draw 0, height-1, '+'
+    frame.draw width-1, 0, '+'
+    frame.draw width-1, height-1, '+'
 
-  # draw corners
-  frame.draw 0, 0, '+'
-  frame.draw 0, height-1, '+'
-  frame.draw width-1, 0, '+'
-  frame.draw width-1, height-1, '+'
-
-  TerminalGameEngine::Input.call do |key|
-    case key
-    when 'q'.ord, TerminalGameEngine::Input::Keys::ESCAPE, TerminalGameEngine::Input::Keys::CTRL_C
-      exit
-    else
-      frame.draw_center 1, key.to_s
+    TerminalGameEngine::Input.call do |key|
+      case key
+      when 'q'.ord, TerminalGameEngine::Input::Keys::ESCAPE, TerminalGameEngine::Input::Keys::CTRL_C
+        exit
+      else
+        frame.draw_center 1, key.to_s
+      end
     end
-  end
 
-  frame.render
+    frame.render
+  end
 end
--- lib/terminal_game_engine.rb
+++ lib/terminal_game_engine.rb
@@ -1,4 +1,15 @@
 Dir[File.dirname(__FILE__) + "/**/*.rb"].each { |f| require f }
 
 module TerminalGameEngine
+  class EngineProxy
+    def on_tick(&block)
+      TerminalGameEngine::Engine.tick &block
+    end
+  end
+
+  def self.run(&block)
+    TerminalGameEngine::Frame.setup
+
+    EngineProxy.new.instance_eval &block
+  end
 end
