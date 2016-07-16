6c8b9449c032b5179b3019865014f1e1ca28a304
Odin Dutton <odindutton@gmail.com>
2016-07-16 10:13:56 +1000

    Allow setting tick_sleep from TerminalGameEngine.run

--- lib/terminal_game_engine.rb
+++ lib/terminal_game_engine.rb
@@ -2,18 +2,30 @@ Dir[File.dirname(__FILE__) + "/**/*.rb"].each { |f| require f }
 
 module TerminalGameEngine
   class EngineProxy
+    def initialize(tick_sleep: nil)
+      @tick_sleep = tick_sleep
+    end
+
     def on_tick(&block)
-      TerminalGameEngine::Engine.tick &block
+      TerminalGameEngine::Engine.tick *tick_args, &block
     end
 
     def on_input(&block)
       TerminalGameEngine::Input.call &block
     end
+
+    private
+
+    def tick_args
+      args = []
+      args << ({tick_sleep: @tick_sleep}) if @tick_sleep
+      args
+    end
   end
 
-  def self.run(&block)
+  def self.run(*args, &block)
     TerminalGameEngine::Frame.setup
 
-    EngineProxy.new.instance_eval &block
+    EngineProxy.new(*args).instance_eval &block
   end
 end
